78bcef0c3d1852b0e1bd4894b92f81fbf8572f43
matheus <barros.matheusprestes@gmail.com>
2020-03-28 19:52:01 -0400

    Mudanças na StarWarsApi.kt e ApiModule.kt
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- app/src/main/java/com/matheus/starwars/api_endpoint/StarWarsApi.kt
+++ app/src/main/java/com/matheus/starwars/api_endpoint/StarWarsApi.kt
@@ -1,9 +1,22 @@
 package com.matheus.starwars.api_endpoint
 import com.matheus.starwars.model.PeopleResult
+import com.matheus.starwars.service.NetworkService
 import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface StarWarsApi {
     @GET("api/people")
     fun getPeople(): Single<PeopleResult>
+    companion object {
+        val instance: StarWarsApi
+            get() =  Retrofit.Builder()
+                .baseUrl(NetworkService.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(StarWarsApi::class.java)
+    }
 }
--- app/src/main/java/com/matheus/starwars/di/ApiModule.kt
+++ app/src/main/java/com/matheus/starwars/di/ApiModule.kt
@@ -8,23 +8,13 @@ import com.matheus.starwars.service.NetworkService
 import dagger.Module
 import dagger.Provides
 import io.reactivex.disposables.CompositeDisposable
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 @Module
 class ApiModule {
 
     @Provides
-    fun provideStarWarsApi(): StarWarsApi {
-        return Retrofit.Builder()
-            .baseUrl(NetworkService.BASE_URL)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-            .create(StarWarsApi::class.java)
-    }
+    fun provideStarWarsApi() = StarWarsApi.instance
 
     @Provides
     fun providePeopleList(): ArrayList<People> {
