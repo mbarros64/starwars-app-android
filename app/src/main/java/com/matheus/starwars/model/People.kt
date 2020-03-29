3edcab92f4a56cfc87ea422b650d7aa67f7416c3
matheus <barros.matheusprestes@gmail.com>
2020-03-29 09:23:20 -0400

    Consertando o code style em People.kt.
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- app/src/main/java/com/matheus/starwars/model/People.kt
+++ app/src/main/java/com/matheus/starwars/model/People.kt
@@ -1,20 +1,22 @@
 package com.matheus.starwars.model
 
+import com.squareup.moshi.Json
+
 data class People(
-    val birth_year: String,
-    val created: String,
-    val edited: String,
-    val eye_color: String,
-    val films: List<String>,
-    val gender: String,
-    val hair_color: String,
-    val height: String,
-    val homeworld: String,
-    val mass: String,
-    val name: String,
-    val skin_color: String,
-    val species: List<String>,
-    val starships: List<String>,
-    val url: String,
-    val peoples: List<String>
+    @field:Json(name = "birth_year") val birth_year: String,
+    @field:Json (name = "created") val created: String,
+    @field:Json (name = "edited") val edited: String,
+    @field:Json (name = "eye_color") val eye_color: String,
+    @field:Json (name = "films") val films: List<String>,
+    @field:Json (name = "gender") val gender: String,
+    @field:Json (name = "hair_color") val hair_color: String,
+    @field:Json (name = "height") val height: String,
+    @field:Json (name = "homeworld")val homeworld: String,
+    @field:Json (name = "mass") val mass: String,
+    @field:Json (name = "name") val name: String,
+    @field:Json (name = "skin_color") val skin_color: String,
+    @field:Json (name = "species") val species: List<String>,
+    @field:Json (name = "starships")val starships: List<String>,
+    @field:Json (name = "url") val url: String,
+    @field:Json (name = "peoples") val peoples: List<String>
 )
