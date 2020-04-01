c8ef8df605c3f2a57b11a19cddcf2080dcb0a1bd
matheus <barros.matheusprestes@gmail.com>
2020-04-01 10:36:29 -0400

    Alterando os properties de People.kt e arrumando o XML quebrado.
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- app/src/main/java/com/mbarros64/starwars/model/People.kt
+++ app/src/main/java/com/mbarros64/starwars/model/People.kt
@@ -3,18 +3,18 @@ package com.mbarros64.starwars.model
 import com.squareup.moshi.Json
 
 data class People(
-    @field:Json(name = "birth_year") val birth_year: String,
+    @field:Json(name = "birth_year") val birthYear: String,
     @field:Json (name = "created") val created: String,
     @field:Json (name = "edited") val edited: String,
-    @field:Json (name = "eye_color") val eye_color: String,
+    @field:Json (name = "eye_color") val eyeColor: String,
     @field:Json (name = "films") val films: List<String>,
     @field:Json (name = "gender") val gender: String,
-    @field:Json (name = "hair_color") val hair_color: String,
+    @field:Json (name = "hair_color") val hairColor: String,
     @field:Json (name = "height") val height: String,
     @field:Json (name = "homeworld")val homeworld: String,
     @field:Json (name = "mass") val mass: String,
     @field:Json (name = "name") val name: String,
-    @field:Json (name = "skin_color") val skin_color: String,
+    @field:Json (name = "skin_color") val skinColor: String,
     @field:Json (name = "species") val species: List<String>,
     @field:Json (name = "starships")val starships: List<String>,
     @field:Json (name = "url") val url: String,
